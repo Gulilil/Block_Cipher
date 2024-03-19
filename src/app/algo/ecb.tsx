8d201e491ac2a2aa602a3051fa8bb90c780577a6
Nicholas Liem <nicholas.liem@shippit.com>
2024-03-19 07:18:48 +0700

    feat: ecb logic

--- src/app/algo/ecb.tsx
+++ src/app/algo/ecb.tsx
@@ -1,11 +1,23 @@
+import { d_function, e_function } from "./hehehe";
+
 export const encryptECB = (arrBlocks: Array<string>, key: string) => {
-  // TO DO : Encryption Algo
-  var res = [""];
+  var res = [];
+
+  for (let i = 0; i < arrBlocks.length; i++) {
+    var currentBlock = arrBlocks[i];
+    var encryptedBlock = e_function(currentBlock, key);
+    res.push(encryptedBlock);
+  }
+
   return res;
 };
 
 export const decryptECB = (arrBlocks: Array<string>, key: string) => {
-  // TO DO : Decryption Algo
-  var res = [""];
+  var res = [];
+  for (let i = 0; i < arrBlocks.length; i++) {
+    var currentBlock = arrBlocks[i];
+    var decryptedBlock = d_function(currentBlock, key);
+    res.push(decryptedBlock);
+  }
   return res;
 };
